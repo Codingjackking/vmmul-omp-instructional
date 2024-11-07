234c3abe78887adf707da9cdf29d60ca29d117a7
Codingjackking <nainghtet123@gmail.com>
2024-11-06 22:02:07 -0800

    updated openmp

--- build/CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o: \
- /global/homes/k/kingjack/vmmul-omp-instructional/dgemv-basic.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o: \
- /global/homes/k/kingjack/vmmul-omp-instructional/dgemv-vectorized.cpp \
- /usr/include/stdc-predef.h
